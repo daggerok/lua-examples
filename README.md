b2536679f52390c9f95c52b1dfe9f8f58742af13
Maksim Kostromin <daggerok@gmail.com>
2020-04-30 23:45:07 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# play with lua
+
+```bash
+brew reinstall lua
+lua template/main.lua
+```
---
+++ template/main.lua
@@ -0,0 +1,16 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: mkostromin
+-- Date: 30.04.2020
+-- Time: 23:02
+-- To change this template use File | Settings | File Templates.
+--
+function main()
+    local time = os.time()
+    print(time)
+
+    local message = "Hello, World!"
+    print(message)
+end
+
+main()
